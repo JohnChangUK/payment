21ec526cff51b63accae289220a6f40fd7f582a5
John Chang <johnchang9094@gmail.com>
2019-02-27 23:46:40 +0000

    Add Equals and Hash Code

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -14,6 +14,7 @@ import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import proxy.Proxy;
 
 import java.math.BigInteger;
+import java.util.Objects;
 
 public class User<T extends Proxy> {
 
@@ -46,6 +47,7 @@ public class User<T extends Proxy> {
                 TransferRequest request = createTransferRequest(
                         "0xd20973DEE7602f8AEA53ea2520266Dd7C16FCd4D", "10000");
                 TransactionReceipt receipt = proxy.executeTransferRequest(request);
+
                 TransactionData transactionData = creationTransactionData(
                         receipt.getTransactionHash(), receipt.getBlockHash(), receipt.getGasUsed());
                 System.out.println(transactionData);
@@ -54,11 +56,11 @@ public class User<T extends Proxy> {
                         .setContractAddress(contractAddress)
                         .setAccountId("0xd20973DEE7602f8AEA53ea2520266Dd7C16FCd4D")
                         .build();
+
                 BigInteger totalBalance = proxy.balanceRequest(balance);
                 System.out.println("Total balance: " + totalBalance);
             }
         } catch (NotEnoughFundsException e) {
-            System.out.println(e);
             log.error("Error: ", e);
         }
     }
@@ -116,5 +118,33 @@ public class User<T extends Proxy> {
 
     public Currency getCurrency() {
         return currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User<?> user = (User<?>) o;
+        return amount == user.amount &&
+                Objects.equals(name, user.name) &&
+                currency == user.currency &&
+                Objects.equals(proxy, user.proxy) &&
+                Objects.equals(contractAddress, user.contractAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, amount, currency, proxy, contractAddress);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", amount=" + amount +
+                ", currency=" + currency +
+                ", proxy=" + proxy +
+                ", contractAddress='" + contractAddress + '\'' +
+                '}';
     }
 }
